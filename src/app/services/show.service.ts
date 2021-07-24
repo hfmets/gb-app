533993b619fdb66f4b7076490c61c04f7ad8b97d
Hayden Farnsworth <hfmets@gmail.com>
2021-07-24 14:36:57 -0500

    updated cors proxy and removed jsonp

--- src/app/services/show.service.ts
+++ src/app/services/show.service.ts
@@ -11,42 +11,42 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class ShowService {
   regToken?: String;
-  corsPrefix = 'https://cors-anywhere.herokuapp.com/';
+  corsPrefix = 'https://mysterious-dawn-78553.herokuapp.com/';
   authUrl = 'https://www.giantbomb.com/app/myapp/get-result?regCode=';
   showCategoryUrl = 'https://www.giantbomb.com/api/video_shows/?api_key=';
   videosUrl = 'https://www.giantbomb.com/api/videos/?api_key=';
 
   getShowTitles(): Observable<Array<Show>> {
     return this.http
-      .jsonp(
+      .get(
+        this.corsPrefix +
         this.showCategoryUrl +
           this.cookie.get('userKey') +
-          '&format=jsonp&sort=id:asc',
-          'json_callback'
+          '&format=json&sort=id:asc'
       )
       .pipe(map((res: any) => res.results));
   }
 
   getVideosOfShow(id: Number): Observable<Array<Video>> {
     return this.http
-      .jsonp(
+      .get(
+        this.corsPrefix +
         this.videosUrl +
           this.cookie.get('userKey') +
-          '&format=jsonp&sort=id:desc&filter=video_show:' +
-          id.toString(),
-          'json_callback'
+          '&format=json&sort=id:desc&filter=video_show:' +
+          id.toString()
       )
       .pipe(map((res: any) => res.results));
   }
 
   getTotalNumberOfVideos(id: number): Observable<number> {
     return this.http
-      .jsonp(
+      .get(
+        this.corsPrefix +
         this.videosUrl +
           this.cookie.get('userKey') +
-          '&format=jsonp&filter=video_show:' +
-          id.toString(),
-          'json_callback'
+          '&format=json&filter=video_show:' +
+          id.toString()
       )
       .pipe(map((res: any) => res.number_of_total_results));
   }
@@ -56,25 +56,25 @@ export class ShowService {
     id: number
   ): Observable<Array<Video>> {
     return this.http
-      .jsonp(
+      .get(
+        this.corsPrefix +
         this.videosUrl +
           this.cookie.get('userKey') +
-          '&format=jsonp&sort=id:desc&filter=video_show:' +
+          '&format=json&sort=id:desc&filter=video_show:' +
           id.toString() +
           '&limit=' +
           pageableData.limit?.toString() +
           '&offset=' +
-          pageableData.offset?.toString(),
-          'json_callback'
+          pageableData.offset?.toString()
       )
       .pipe(map((res: any) => res.results));
   }
 
   getUserRegToken(userCode: String): Observable<any> {
     return this.http
-      .jsonp(
-        this.authUrl + userCode + '&format=jsonp',
-        'json_callback'
+      .get(
+        this.corsPrefix +
+        this.authUrl + userCode + '&format=json'
       )
       .pipe(map((res: any) => res));
   }
